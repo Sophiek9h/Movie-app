b9a1b2b9a8076b86438879a3e4819efcb71e5b61
Sophie kalagbor <sophiek9h@gmail.com>
2025-07-20 20:38:27 +0100

    formats

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,88 +1,80 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
 
-
 export default function Index() {
-
   const router = useRouter();
 
-  const { data: movies, loading, error } = useFetch(() => fetchMovies({
-    query: ""
-  })); 
+  const {
+    data: movies,
+    loading,
+    error,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
 
   return (
     <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
 
-      <Image
-      source={images.bg} 
-      className="absolute w-full z-0"     
-      />
+      <ScrollView
+        className="flex-1 px-2"
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-      <ScrollView className="flex-1 px-2" 
-      showsHorizontalScrollIndicator={false} 
-      showsVerticalScrollIndicator={false} 
-      contentContainerStyle={{ minHeight: "100%" , paddingBottom: 10}}>
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-
-        { loading ? (
-          <ActivityIndicator
-            size="large"
-            color="#0000ff"
-            className="mt-10"
-          />
-        
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" className="mt-10" />
         ) : error ? (
           <Text className="text-red-500 text-center mt-10">
             Error fetching movies: {error.message}
           </Text>
         ) : (
-
           <View className="flex-1 mt-5">
-            <SearchBar 
+            <SearchBar
               onPress={() => router.push("/search")}
               placeholder="Search for a Movie"
             />
 
             <>
-              <Text
-                className="text-white text-lg font-semibold mt-5 mb-3" 
-                >
-                  Latest Movies
-                </Text>
+              <Text className="text-white text-lg font-semibold mt-5 mb-3">
+                Latest Movies
+              </Text>
 
-                <FlatList
-                  data={movies}
-                  renderItem={({ item }) => (
-                      <MovieCard
-                       {...item}
-                      />
-                  )}
-                  keyExtractor={(item) => item.id.toString()}
-                  numColumns={3}
-                  columnWrapperStyle={{ 
-                    justifyContent: "flex-start",
-                    gap: 20, 
-                    marginBottom: 10
-                  }}
-                  scrollEnabled={false}
-                  className="mt-2 pb-32"
-                  showsVerticalScrollIndicator={false}
-                />
-                  
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  marginBottom: 10,
+                }}
+                scrollEnabled={false}
+                className="mt-2 pb-32"
+                showsVerticalScrollIndicator={false}
+              />
             </>
-        </View>
-
+          </View>
         )}
-
       </ScrollView>
-
     </View>
-
   );
 }
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,6 @@
-import { icons } from '@/constants/icons'
-import React from 'react'
-import { Image, TextInput, View } from 'react-native'
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
 
 interface Props {
   onPress?: () => void;
@@ -9,22 +9,25 @@ interface Props {
   onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({onPress, placeholder, value, onChangeText}: Props) => {
+const SearchBar = ({ onPress, placeholder, value, onChangeText }: Props) => {
   return (
-    <View className='flex-row items-center bg-dark rounded-full px-5 py-4'>
-      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff"/>
-      <TextInput 
-            placeholder={placeholder}
-            onPress={onPress}
-            value={value}
-            onChangeText={onChangeText}
-            placeholderTextColor="ab8bff" 
-            className='flex-1 text-white ml-3'  
-        />
-
-
+    <View className="flex-row items-center bg-dark rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        placeholder={placeholder}
+        onPress={onPress}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor="ab8bff"
+        className="flex-1 text-white ml-3"
+      />
     </View>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
